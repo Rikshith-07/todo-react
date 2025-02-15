c4111ed1180fa6d8356ee05883f757548d0ef0b5
Hithendra <hithendrasai2@gmail.com>
2025-02-15 23:20:12 +0530

    Updates

---
+++ precache-manifest.8ddd3a35db85fa9471a37ff3ee561c41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8ef309457e8e39b857cdaba7c8c492",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "60ff326f39bc02b875c0",
+    "url": "/todo-react/static/css/main.465be72c.chunk.css"
+  },
+  {
+    "revision": "b8019a967dfe4014221c",
+    "url": "/todo-react/static/js/2.04d82739.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/todo-react/static/js/2.04d82739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710a8c553ded4b6c8aa6",
+    "url": "/todo-react/static/js/3.e25e74c1.chunk.js"
+  },
+  {
+    "revision": "60ff326f39bc02b875c0",
+    "url": "/todo-react/static/js/main.7a7cb9ee.chunk.js"
+  },
+  {
+    "revision": "0aa419dfef5a1456cf4b",
+    "url": "/todo-react/static/js/runtime-main.57bd69d6.js"
+  }
+]);
